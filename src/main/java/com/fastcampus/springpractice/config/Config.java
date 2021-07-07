5467f6bf4d6d63d26a0ebc3a7ebda7fc94f620d5
Uno <djkehh@gmail.com>
2021-07-08 04:53:25 +0900

    Implement #1 - WebApplicationInitializer
    기초 예제를 이용해 웹 mvc 진입점 코드와
    web mvc 기본 설정을 구현

--- src/main/java/com/fastcampus/springpractice/config/Config.java
+++ src/main/java/com/fastcampus/springpractice/config/Config.java
@@ -2,7 +2,9 @@ package com.fastcampus.springpractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.fastcampus.springpractice")
 @Configuration
 public class Config {
---
+++ src/main/java/com/fastcampus/springpractice/web/MyWebApplicationInitializer.java
@@ -0,0 +1,26 @@
+package com.fastcampus.springpractice.web;
+
+import com.fastcampus.springpractice.config.Config;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        // Load Spring web application configuration
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(Config.class);
+
+        // Create and register the DispatcherServlet
+        DispatcherServlet servlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+
+}
